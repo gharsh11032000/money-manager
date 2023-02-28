d10a9f61bef0805da16a5e9e295bd9efbd341824
Harsh Gupta <gharsh11032000@gmail.com>
2023-02-28 10:17:47 +0530

    fixing some issues in server.js

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ app.use("/api/transactions", require("./router/transactionRoute"));
 // Serve Frontend
 if (process.env.NODE_ENV === "production") {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../money-manager/dist")));
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../money-manager/dist/index.html"));
   });
 } else {
   app.get("/", async (req, res) => {
