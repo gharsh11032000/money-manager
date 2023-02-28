02762738f498162e67e4646c9492fd2a9f2779ca
Harsh Gupta <gharsh11032000@gmail.com>
2023-02-28 11:37:20 +0530

    Fixing axios issue

--- money-manager/src/context/AuthContext.jsx
+++ money-manager/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
-import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
+import axios from "@bundled-es-modules/axios/axios.js";
 
 export const AuthContext = createContext();
 
